af4c2eb2ad8c0031a1cc61654d2576f010f75c08
PiotrSter <47736359+PiotrSter@users.noreply.github.com>
2020-07-02 16:59:07 +0200

    Initial commit

---
+++ Assets/Scripts/CameraFollowPlayer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowPlayer : MonoBehaviour
+{
+    public Transform playerTransform;
+    private float cameraOffset = -10f;
+
+    void Update()
+    {
+        transform.position = new Vector3(playerTransform.position.x, playerTransform.position.y, cameraOffset);
+    }
+}
---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float planeSpeed = 20.0f;
+    public float planeSpeedRotate = 1.0f;
+    private Vector3 planePosition;
+
+    private void Awake()
+    {
+        rb = this.gameObject.GetComponent<Rigidbody2D>();
+        planePosition = this.gameObject.transform.position;
+    }
+
+    void Update()
+    {
+        
+        if (Input.GetKey(KeyCode.W))
+        {
+            this.rb.AddForce(transform.up * planeSpeed);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            this.gameObject.transform.Rotate(new Vector3(0, 0, planeSpeedRotate));
+        }       
+        if (Input.GetKey(KeyCode.D))
+        {
+            this.gameObject.transform.Rotate(new Vector3(0, 0, -planeSpeedRotate));
+        }
+        
+        /*
+        if (Input.GetKey(KeyCode.W))
+        {
+            planePosition.y +=  planeSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            this.gameObject.transform.Rotate(new Vector3(0, 0, planeSpeedRotate));
+            //planePosition.x += -planeSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            this.gameObject.transform.Rotate(new Vector3(0, 0, -planeSpeedRotate));
+            //planePosition.x += planeSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            planePosition.y += -planeSpeed * Time.deltaTime;
+        }
+        */
+        //this.gameObject.transform.position = planePosition;
+        //planePosition.x = Mathf.Clamp(planePosition.x, -29.1f, 30f); 
+        //planePosition.x += Input.GetAxis("Horizontal") * planeSpeed * Time.deltaTime;
+        //planePosition.y += Input.GetAxis("Vertical") * planeSpeed * Time.deltaTime;
+        //planePosition.y = Mathf.Clamp(planePosition.y, -18.8f, 12.7f);
+        //this.gameObject.transform.position = planePosition;
+        
+        
+    }
+}
