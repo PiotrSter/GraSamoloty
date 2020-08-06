1a794ba1dde7d64a40df170f3b52cc57fe473878
PiotrSter <47736359+PiotrSter@users.noreply.github.com>
2020-08-06 17:46:06 +0200

    layer

--- Assets/Scripts/EnemyShotSpawner.cs
+++ Assets/Scripts/EnemyShotSpawner.cs
@@ -9,21 +9,28 @@ public class EnemyShotSpawner : MonoBehaviour
     private GameObject Enemy;
     public float bulletDelay = 2f;
     public float shotDelay;
+    //LayerMask PlayerLayer;
 
     private void Awake()
     {
         this.Player = GameObject.Find("Player");
         this.Enemy = GameObject.Find("StandardEnemy");
-    }
 
+    }
+    /*
+    void Start()
+    {
+        PlayerLayer = LayerMask.NameToLayer("Player");
+    }
+    */
     void FixedUpdate()
     {
         int layerMask = 1 << 8;
-        //RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.up, Mathf.Infinity, layerMask);
-        Ray ray = new Ray(transform.position, Vector2.up);
-        RaycastHit hitInfo;
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.up, 100, layerMask);
+        //Ray ray = new Ray(transform.position, Vector2.up);
+        //RaycastHit hitInfo;
         float distance = Vector2.Distance(Enemy.transform.position, Player.transform.position);        
-        if (Physics.Raycast(ray, out hitInfo, Mathf.Infinity, layerMask))
+        if (hit.collider != null)
         {
             shotDelay -= Time.deltaTime;
             if (shotDelay <= 0)
